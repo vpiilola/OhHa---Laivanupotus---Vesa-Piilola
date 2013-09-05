d968e252c7e046058afd077e81302a3b25ee18f3
Vesa M S Piilola <vpiilola@dhp-20.cs.helsinki.fi>
2013-09-05 15:20:33 +0300

    Ohjelmoinnin aloitus pienistä luokista

--- Laivanupotus/src/laivanupotus/Laivanupotus.java
+++ Laivanupotus/src/laivanupotus/Laivanupotus.java
@@ -14,6 +14,9 @@ public class Laivanupotus {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Pelaaja Risto = new Pelaaja("Risto");
+       
+        System.out.println(Risto.getNimi());
+        
     }
 }
