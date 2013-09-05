7e026d7b3588431d2527265628ed28986823cf84
Vesa M S Piilola <vpiilola@dhp-20.cs.helsinki.fi>
2013-09-05 15:23:46 +0300

    Ensimmäiset pienet luokat

---
+++ Laivanupotus/src/laivanupotus/Pelaaja.java
@@ -0,0 +1,22 @@
+package laivanupotus;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vpiilola
+ */
+public class Pelaaja {
+    private String name;
+    
+    public Pelaaja(String nimi){    //halutaa siis luoda kaksi pelaajaa jokaista
+        this.name=nimi;             //peliä kohti
+    }
+    
+    public String getNimi(){        //getteri jota myöhemmin sovellus varmaan
+        return this.name;           // haluaa käyttää
+    }
+}
---
+++ Laivanupotus/src/laivanupotus/Pelialusta.java
@@ -0,0 +1,19 @@
+package laivanupotus;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vpiilola
+ */
+public class Pelialusta {
+    private int koko;
+    
+    public Pelialusta(){    //Pelialusta luodaan jokaista pelaajaa kohti?
+        this.koko=100;       //Onko tarpeen muokata kokoa?
+    }
+    
+}
